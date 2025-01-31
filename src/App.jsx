b1567b2fa286f756eccced535e80f37a4817b3d5
Warren Brown <warrengbrn@gmail.com>
2025-01-31 08:58:47 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { setAssetPath as setCalciteComponentsAssetPath } from '@esri/calcite-components/dist/components';
+setCalciteComponentsAssetPath("https://js.arcgis.com/calcite-components/2.13.2/assets");
+
+import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/dist/components/arcgis-legend";
+import "@arcgis/map-components/dist/components/arcgis-search";
+import "@arcgis/map-components/dist/components/arcgis-zoom";
+
+export default function App() {
+    return (
+        <arcgis-map
+            itemId="d5dda743788a4b0688fe48f43ae7beb9"
+            onarcgisViewReadyChange={(event) => {
+                console.log("MapView ready", event);
+              }}
+        >
+            <arcgis-search position="top-right"></arcgis-search>
+            <arcgis-legend position="bottom-left"></arcgis-legend>
+            <arcgis-zoom position="bottom-right"></arcgis-zoom>
+        </arcgis-map>
+    )
+}
